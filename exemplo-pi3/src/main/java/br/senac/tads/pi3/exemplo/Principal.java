148074affea6db8e580fc3b1f4d375af4df9cfed
GEO INFORMACAO <geoinformacao@senacsp.edu.br>
2020-02-20 11:00:51 -0300

    Mudança no código

--- exemplo-pi3/src/main/java/br/senac/tads/pi3/exemplo/Principal.java
+++ exemplo-pi3/src/main/java/br/senac/tads/pi3/exemplo/Principal.java
@@ -13,6 +13,10 @@ public class Principal {
     
     public static void main(String[] args) {
         System.out.println("Exemplo PI3");
+        
+        for(int i = 0; i < 20 ; i++){
+            System.out.println("Hello World");
+        }
     }
     
 }
